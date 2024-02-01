c1ceda5645c9bface344a2dc75c5f075b9d58476
karthik reddy <mekalakarthikreddy321@gmail.com>
2024-02-01 17:03:24 +0530

    db connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 import  express from "express";
-
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
 const app = express();
+dotenv.config();
+mongoose.connect(process.env.MONGO)
+.then( () => {
+    console.log("mongoose connected succesfully");
+}).catch((error) =>{
+    console.log("failed to connect db")
+})
 
 
 app.listen(3000 , ()=>{
